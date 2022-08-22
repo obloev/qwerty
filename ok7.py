02ceb2bd35941c8a25507032740ab527f4cd1bd0
Obloev Komronbek <89721936+obloev@users.noreply.github.com>
2022-08-22 11:33:55 +0500

    Create ok7.py

---
+++ ok7.py
@@ -0,0 +1,19 @@
+def f(n):
+    return sum(list(map(int, str(n))))
+
+def check(n):
+    return n % f(n)**2 == 0
+
+def get(n):
+    s = i = 0
+    
+    while s < n:
+        i += 1
+        if check(i):
+            s += 1
+    
+    return i
+
+
+n = int(input())
+print(get(n))
